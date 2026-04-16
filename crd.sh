a0760ab691a81c89cb5ba4ca2b8686f28bb2dd5b
andrygibhq02 <andrygibhq02@gmail.com>
2026-04-16 19:07:16 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -12,29 +12,29 @@ Pin=123456
 
 installCRD() {
     echo "Installing Remote Desktop..."
-    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
-    sudo dpkg --install chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
-    sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
+    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+    sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+    sudo apt install --assume-yes --fix-broken
     echo "installation complete!"
 }
 
 installDesktopEnvironment() {
     echo "Installing Desktop Environment..."
-    sudo apt install --assume-yes xfce4 xfce4-goodies > /dev/null 2>&1
+    sudo apt install --assume-yes xfce4 xfce4-goodies
     echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
     chmod +x ~/.chrome-remote-desktop-session
-    sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+    sudo apt remove --assume-yes gnome-terminal
     echo "installation complete!"
 }
 
 installBrowser() {
     echo "Installing Google Chrome..."
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-    sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-    sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
-    sudo apt install --assume-yes remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret > /dev/null 2>&1
-    sudo apt install --assume-yes python3-pip > /dev/null 2>&1
-    sudo pip install gdown > /dev/null 2>&1
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt install --assume-yes --fix-broken
+    sudo apt install --assume-yes remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret
+    sudo apt install --assume-yes python3-pip
+    sudo pip install gdown
     echo "installation complete!"
 }
 
